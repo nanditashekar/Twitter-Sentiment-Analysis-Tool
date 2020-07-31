b2e9e5f7a4b7856f6f26f80181fc9bfc10440c73
Nandita S <68863983+nanditashekar@users.noreply.github.com>
2020-07-30 22:17:48 -0700

    Create importlibs.py

---
+++ importlibs.py
@@ -0,0 +1,17 @@
+#Data Analysis
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+#Data Preprocessing and Feature Engineering
+from textblob import TextBlob
+import re
+from nltk.corpus import stopwords
+from nltk.stem.wordnet import WordNetLemmatizer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+
+#Model Selection and Validation
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import confusion_matrix, classification_report,accuracy_score
